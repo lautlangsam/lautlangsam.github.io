d31ee4481c4694592439b4c0effe3007e6e4943c
lola.sanglante <lola.gingras@protonmail.com>
2021-10-12 14:13:09 -0400

    dynamically load webiste sections

---
+++ script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+    $(".content-to-load").load("./home.html")
+})
+
+$("#nav-home").click(function() {
+    $(".content-to-load").load("./home.html");
+})
+
+$("#nav-tour").click(function() {
+    $(".content-to-load").load("./tour.html");
+})
+
+$("#nav-news").click(function() {
+    $(".content-to-load").load("./news.html");
+})
+
+$("#nav-about").click(function() {
+    $(".content-to-load").load("./about.html");
+})
+
+$("#nav-merch").click(function() {
+    $(".content-to-load").load("./merch.html");
+})
+
+
